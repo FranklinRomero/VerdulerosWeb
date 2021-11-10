777d04408bed9552f54f0dc6217f5072ed154c22
Franklin <a2020115522@estudiantes.upsa.edu.bo>
2021-11-10 18:13:56 -0400

    Migrations implementado

---
+++ WebVerduleros/Migrations/20211110221339_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebVerduleros.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Verduras",
+                columns: table => new
+                {
+                    VerduraId = table.Column<int>(type: "int", maxLength: 1, nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Verdura = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Verduras", x => x.VerduraId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Verduras");
+        }
+    }
+}
