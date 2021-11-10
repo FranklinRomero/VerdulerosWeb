cbfd18020d7cf196d2ae1adec0f898334ccb684c
Franklin <a2020115522@estudiantes.upsa.edu.bo>
2021-11-10 17:49:50 -0400

    Clase Verduras creada exitosamente

---
+++ WebVerduleros/Models/Verduras.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebVerduleros.Models
+{
+    public class Verduras
+    {
+        [Key]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public int VerduraId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre del item")]
+        public string Verdura { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(3, MinimumLength = 1, ErrorMessage = "La longitud de {0} debe estar entre {1} y {3}")]
+        public int Cantidad { get; set; }
+
+    }
+}
